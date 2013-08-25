e3314d0e6e201a0615f4a7d40b8c233d1e9d34a0
Sharipov Ruslan <sharipov.ru@gmail.com>
2013-08-25 17:48:39 +0700

    aaaa

--- js/game.js
+++ js/game.js
@@ -25,7 +25,7 @@ var game = {
         // Load the resources.
         me.loader.preload(game.resources);
 
-        me.debug.renderHitBox = true;
+        // me.debug.renderHitBox = true;
 
         // Initialize melonJS and display a loading screen.
         me.state.change(me.state.LOADING);
