96d7c3e601d8494cad58b8b6e514da56807361b5
Alexander Belozerov <belozerov.sasha@gmail.com>
2013-08-25 13:06:43 +0700

    bind jump to both X and DOWN keys

--- js/game.js
+++ js/game.js
@@ -60,6 +60,7 @@ var game = {
        me.input.bindKey(me.input.KEY.RIGHT, "right");
        me.input.bindKey(me.input.KEY.X,     "jump", true);
        me.input.bindKey(me.input.KEY.DOWN,  "down");
+       me.input.bindKey(me.input.KEY.UP,    "jump", true);
           
        // start the game 
        me.state.change(me.state.MENU);
