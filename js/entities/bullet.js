3d63198ffbc2378e24cf3f9877d30ac024c5c3a2
Anna Tarasenko <annie.tarasenko@gmail.com>
2013-08-25 15:28:54 +0700

    Deletions

--- js/entities/bullet.js
+++ js/entities/bullet.js
@@ -18,16 +18,22 @@ var bullet = me.ObjectEntity.extend({
 
         this.vel.x = this.initVelocity.x + 15;
 
-        this.olderX = this.oldX
-        this.oldX = this.prevX
-        this.prevX = this.pos.x
+        this.olderX = this.oldX;
+        this.oldX = this.prevX;
+        this.prevX = this.pos.x;
 
         // check for collision
         var res = me.game.collide(this);
         if (res) {
             if (res.obj.type == me.game.ENEMY_OBJECT) {
+                poo = new game.PooEntity(res.obj.pos.x, res.obj.pos.y + 60, this.vel, {image: 'poo', spritewidth: 44});
+                me.game.add(poo, res.obj.z);
+                me.game.sort();
                 me.game.remove(res.obj);
                 me.game.remove(this);
+            /*}  else if (res.obj.type == me.game.ENEMY2_OBJECT) {
+                me.game.remove(res.obj);
+                me.game.remove(this);*/
             } else if (res.obj.type !== 0) {
                 me.game.remove(this);
             }
--- js/screens/win.js
+++ js/screens/win.js
@@ -1,85 +1,3 @@
-game.TitleScreen = me.ScreenObject.extend({
-    // constructor
-    init: function() {
-        this.parent(true);
- 
-        // title screen image
-        this.title = null;
- 
-        this.font = null;
-        this.scrollerfont = null;
-        this.scrollertween = null;
- 
-        this.scroller = "LET'S TRY TO MAKE A WORLD SHIT!";
-        this.scrollerpos = 600;
-    },
- 
-    // reset function
-    onResetEvent: function() {
-        if (this.title == null) {
-            // init stuff if not yet done
-            this.title = me.loader.getImage("title_screen");
-        }
-        if (this.font == null) {
-            // font to display the menu items
-            this.font = new me.BitmapFont("32x32_font", 32);
- 
-            // set the scroller
-            this.scrollerfont = new me.BitmapFont("32x32_font", 32);
-        }
- 
-        // reset to default value
-        this.scrollerpos = 640;
- 
-        // a tween to animate the arrow
-        this.scrollertween = new me.Tween(this).to({
-            scrollerpos: -2200
-        }, 10000).onComplete(this.scrollover.bind(this)).start();
- 
-        // enable the keyboard
-        me.input.bindKey(me.input.KEY.ENTER, "enter", true);
- 
-        // play something
-        //me.audio.play("cling");
- 
-    },
- 
-    // some callback for the tween objects
-    scrollover: function() {
-        // reset to default value
-        this.scrollerpos = 640;
-        this.scrollertween.to({
-            scrollerpos: -2200
-        }, 10000).onComplete(this.scrollover.bind(this)).start();
-    },
- 
-    // update function
-    update: function() {
-        // enter pressed ?
-        if (me.input.isKeyPressed('enter')) {
-            me.state.change(me.state.PLAY);
-        }
-        return true;
-    },
- 
-    // draw function
-    draw: function(context) {
-        context.drawImage(this.title, 0, 0);
- 
-        this.font.draw(context, "PRESS ENTER TO PLAY", 336, 575);
-        this.scrollerfont.draw(context, this.scroller, this.scrollerpos, 650);
-    },
- 
-    // destroy function
-    onDestroyEvent: function() {
-        me.input.unbindKey(me.input.KEY.ENTER);
- 
-        //just in case
-        this.scrollertween.stop();
-    }
- 
-});
-
 game.WinScreen = me.ScreenObject.extend({
     // constructor
     init: function() {
@@ -101,7 +19,6 @@ game.WinScreen = me.ScreenObject.extend({
         if (this.title == null) {
             // init stuff if not yet done
             this.title = me.loader.getImage("win_screen");
-            console.log(this.title);
         }
         if (this.font == null) {
             // font to display the menu items
